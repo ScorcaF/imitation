7a57f756f6cd541a800e9a8b04c3955152863702
francesco scorca <70110839+ScorcaF@users.noreply.github.com>
2022-08-09 13:37:05 +0200

    Update wrappers.py

--- src/imitation/data/wrappers.py
+++ src/imitation/data/wrappers.py
@@ -34,12 +34,14 @@ class BufferingWrapper(VecEnvWrapper):
         self.n_transitions = None
 
     def reset(self, **kwargs):
-        if (
-            self._init_reset
-            and self.error_on_premature_reset
-            and self.n_transitions > 0
-        ):  # noqa: E127
-            raise RuntimeError("BufferingWrapper reset() before samples were accessed")
+        
+
+#         if (
+#             self._init_reset
+#             and self.error_on_premature_reset
+#             and self.n_transitions > 0
+#         ):  # noqa: E127
+#             raise RuntimeError("BufferingWrapper reset() before samples were accessed") ########################à
         self._init_reset = True
         self.n_transitions = 0
         obs = self.venv.reset(**kwargs)
